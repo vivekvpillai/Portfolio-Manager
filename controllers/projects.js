9486d4f9f2d6e73ebd889a82d8f53c6db178f859
Vijayan <vijayan@Vijayans-MBP.attlocal.net>
2021-10-25 17:20:42 -0500

    finished applications functionality

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,6 +4,66 @@ const Portfolio = require('../models/portfolio.js')
 const Application = require('../models/applications.js')
 const User = require('../models/users.js')
 
+
+////////////////////////////////////////
+/////////////////APPLICATIONS//////////////
+//Application Data POST
+router.post('/applications', (req,res)=> { //wha's the use of the scond parameter here?
+  Application.create(req.body, (error, createdApplication) => {
+    res.redirect('/portfolio/applications')
+  })
+})
+
+//Application Data Index
+router.get('/applications', (req, res) => {
+  Application.find({}, (error, allApplications) => {
+    res.render('appindex.ejs', {
+      applicationList: allApplications,
+      currentUser: req.session.currentUser
+    })
+  })
+});
+
+//Applications Create
+router.get('/applications/new', (req,res) => {
+  res.render('appnew.ejs', {
+   currentUser: req.session.currentUser
+ })
+})
+
+//Applications delete
+router.delete('/applications/:id', (req,res) => {
+  Application.findByIdAndRemove(req.params.id, (err, data) =>{
+    res.redirect('/portfolio/applications')
+  })
+})
+
+//Applications Show
+router.get('/applications/:id', (req,res) => {
+  Application.findById(req.params.id, (err, foundApplications) => {
+    res.render('appshow.ejs', {
+      application: foundApplications,
+      currentUser: req.session.currentUser
+    })
+  })
+})
+
+//Application Edit
+router.get('/applications/:id/edit', (req,res) =>{
+  Application.findById(req.params.id, (err, foundApplication) => {
+    res.render('appedit.ejs', {
+      application: foundApplication,
+      currentUser: req.session.currentUser
+    })
+  })
+})
+
+/////////////////////////////////////////////////
+//////////////////END OF APPLICATION ROUTES////////
+///////////////////////////////////
+
+
+
 //___________________
 // Routes
 //___________________
@@ -71,43 +131,5 @@ router.get('/:id/edit', (req,res) =>{
   })
 })
 
-////////////////////////////////////////
-/////////////////APPLICATIONS//////////////
-//Application Data Index
-router.get('/applications', (req, res) => {
-  Application.find({}, (error, allApplications) => {
-    res.render('appindex.ejs', {
-      applicationList: allApplications,
-      currentUser: req.session.currentUser
-    })
-  })
-});
-
-//Applications Show
-router.get('/applications/:id', (req,res) => {
-  Application.findById(req.params.id, (err, foundApplications) => {
-    res.render('appshow.ejs', {
-      application: foundApplications,
-      currentUser: req.session.currentUser
-    })
-  })
-})
-
-//Applications Create
-router.get('/applications/new', (req,res) => {
-  res.render('appnew.ejs', {
-   currentUser: req.session.currentUser
- })
-})
-
-//Edit
-router.get('/applications/:id/edit', (req,res) =>{
-  Application.findById(req.params.id, (err, foundApplication) => {
-    res.render('appedit.ejs', {
-      application: foundApplication,
-      currentUser: req.session.currentUser
-    })
-  })
-})
 
 module.exports = router;
